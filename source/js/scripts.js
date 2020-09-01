1ec9c675aa7b0136b2951071e90d6789fd17b001
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2020-09-01 21:47:01 +1000

    попытка исправить пункт 3.2

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -20,18 +20,17 @@ window.addEventListener("keydown", function (evt) {
 
 //Burger
 
-var buttonBurger = document.querySelector(".js-burger");
-var navigationLink = document.querySelector(".navigation__item--off");
+const buttonBurger = document.querySelector(".js-burger");
+const navigationLink = document.querySelector(".navigation__item--off");
 
-buttonBurger.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  navigationLink.classList.toggle('navigation__item--on');
-});
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (modal.classList.contains("navigation__item--on")) {
-      modal.classList.remove("navigation__item--on");
-    }
-  }
+navToggle.addEventListener("click", function () {
+  // console.log(links.classList);
+  // console.log(links.classList.contains("random"));
+  // console.log(links.classList.contains("links"));
+  // if (links.classList.contains("show-links")) {
+  //   links.classList.remove("show-links");
+  // } else {
+  //   links.classList.add("show-links");
+  // }
+  links.classList.toggle("navigation__item--on");
 });
