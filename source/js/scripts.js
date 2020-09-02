39cfbdbc1ff493fe17fdaac78ed03277bcf7216a
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2020-09-02 19:54:09 +1000

    попытка вставить карту

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -18,18 +18,32 @@ window.addEventListener("keydown", function (evt) {
   }
 });
 
-//Burger
-const buttonBurger = document.querySelector(".js-burger");
-const navigationLink = document.querySelector(".navigation__item--off");
+//Map
+if (document.querySelector(".contacts__map")) {
+  var imageMap = document.querySelector(".map__image-wrapper");
 
-buttonBurger.addEventListener("click", function () {
-  // console.log(links.classList);
-  // console.log(links.classList.contains("random"));
-  // console.log(links.classList.contains("links"));
-  // if (links.classList.contains("show-links")) {
-  //   links.classList.remove("show-links");
-  // } else {
-  //   links.classList.add("show-links");
-  // }
-  navigationLink.classList.toggle("navigation__item--on");
-});
+  function initMap() {
+    var uluru = { lat: 59.938939, lng: 30.323186 };
+    var map = new google.maps.Map(document.getElementById("google-map"), {
+      zoom: 17,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      icon: {
+        url: "./img/svg/map-pin.svg",
+        scaledSize: new google.maps.Size(67, 100)
+      }
+    });
+  }
+
+  function hideImageMap() {
+    imageMap.classList.add("visually-hidden");
+  }
+
+  window.onload = hideImageMap;
+  window.addEventListener("load", initMap);
+}
